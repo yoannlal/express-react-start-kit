3fde86fff4254811490fa315938102296862e439
Yoann <yoann.lal@gmail.com>
2016-02-17 15:57:40 +0100

    Create About.js

---
+++ client/components/About.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const About = React.createClass({
+
+  render() {
+    return (
+        <div>
+          <h1>
+            About
+          </h1>
+        </div>
+    );
+  }
+});
+
+module.exports = About;
